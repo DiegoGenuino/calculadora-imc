6d8f47ee8bc6acf705af5d4990e73d609ddcf4a0
--rename <genuinodiego11@gmail.como>
2025-01-29 16:45:56 -0300

    FIX THINGS

--- script.js
+++ script.js
@@ -1,14 +1,16 @@
-const input_peso = parseInt(document.getElementById("peso").value);
-
-const input_altura = parseInt(document.getElementById("altura").value);
-
-const div_resultado = document.getElementById("div-resultado");
-let resultadoImc = input_peso / (input_altura ** 2);
 
 function calcularImc() {
-    div_resultado.innerHTML = `
 
-   <h3>Seu IMC é: ${resultadoImc}</h3>
 
+  const input_peso = parseFloat(document.getElementById("peso").value);
+
+  const input_altura = parseFloat(document.getElementById("altura").value);
+
+  const div_resultado = document.getElementById("div-resultado");
+
+  const resultadoImc = input_peso / (input_altura ** 2);
+
+  div_resultado.innerHTML = `
+   <h3>Seu IMC é: ${resultadoImc.toFixed(2)}</h3>
      `
 }
